2a9b51049255a0766712b4c046131761a1894cfc
Leonardo Silva <leonardo.silva@modalgr.com.br>
2020-03-13 17:47:20 -0300

    Map Marker

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1 align="center">Nxtsh React Native Maps GR 🗺️</h1>
+<h1 align="center">Nxtsh React Native MapsGR 🗺️</h1>
 <p>
   <img alt="Version" src="https://img.shields.io/badge/version-0.0.1-blue.svg?cacheSeconds=2592000" />
 </p>
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import {HomeList} from './components/HomeList';
 import { View } from 'native-base';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { StyleSheet } from 'react-native';
-import {  } from '@modalgr/andromeda';
+import { Alert, StyleSheet, Dimensions } from 'react-native';
+import { Marker } from 'react-native-maps';
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
+    height: Dimensions.get('window').height,
+    height: Dimensions.get('window').width,
     width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
@@ -24,12 +25,18 @@ export function HomeScreen() {
       provider={PROVIDER_GOOGLE}
       style={styles.map}
       region={{
-        latitude: 37.78825,
-        longitude: -122.4324,
+        latitude: -23.9591226,
+        longitude: -46.3311974,
         latitudeDelta: 0.015,
         longitudeDelta: 0.0121,
       }}
     >
+    <Marker
+      title="ModalGR"
+      description="ModalGR no Maps"
+      coordinate={{latitude: -23.9591226, longitude: -46.3311974}}
+      onPress={() => Alert.alert('Pedrinho Baitola')}
+    />
     </MapView>
 
     
