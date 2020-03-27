fbfbb09e7f20f6aeb7e040dc06e6e2b1fdc72017
blzpedro <phenriqmelo99@gmail.com>
2020-03-27 17:54:33 -0300

    mudando de map para flatlist

--- src/screens/Alertas/index.jsx
+++ src/screens/Alertas/index.jsx
@@ -1,10 +1,10 @@
 import React , {useEffect, useState} from 'react';
-import { View, Text, Container, Content, H1, Icon, Item, Input, DatePicker} from 'native-base';
-import { Alert, StyleSheet, Dimensions, ImageBackground } from 'react-native';
+import { View, Container, Content, H1, Icon, Item, Input, DatePicker} from 'native-base';
+import { Alert, StyleSheet, Dimensions, ImageBackground, FlatList, Text } from 'react-native';
 import { Styles } from './styles';
 import { verticalScale, scale } from 'react-native-size-matters';
 import { COLOR } from '../../config/styles';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
 import { Alertas } from './components/Alertas';
 import { api, handleRequestError } from '../../utils/api';
 import { Loader } from '../../components';
@@ -15,7 +15,6 @@ export function AlertaScreen() {
     const [open, setOpen] = useState(false)
     const [search, setSearch] = useState(new Date());
     const [alertas, setAlertas] = useState([]);
-    const checked = false
 
     useEffect(() => {
       getAlertas()
@@ -27,23 +26,22 @@ export function AlertaScreen() {
 
       api('').get(url)
       .then(({ data }) => {
-        setAlertas(data);
+        let arrayAlertas = []
+        data.map((alerta, key) => {
+          arrayAlertas.push(Object.assign(alerta, {'key': `${key}`}))
+        })
+        setAlertas(arrayAlertas)
       })
       .catch(err => console.log('Erro ao get por data', err))
       .finally(() => {
         setOpen(false)
       });
     }
-
-    // function openAccordion(alert) {
-    //   const listaAlertas = Array.from(alertas).filter(item => item.id !== alert.idAlerta);
-    //   listaAlertas.push({ ...alert, checked: !alert.checked })
-    //   alert = {...alert, checked: !alert.checked};
-    // }
     
-  return (
+    console.log(alertas)
+
+  return (  
       <Container>
-          <Content>
             <ImageBackground source={require('../../assets/borda-topo.png')} style={{ width: expanded ? Dimensions.get('window').width : null }}>
             <View style={{ flexDirection: 'row', paddingLeft: scale(20), paddingVertical: verticalScale(20), paddingHorizontal: scale(20) }}>
                 <Icon
@@ -55,8 +53,9 @@ export function AlertaScreen() {
                 <H1 style={{ color: COLOR.ACCENT, fontWeight: 'bold', marginLeft: scale(10) }}>Histórico de Alertas</H1>
             </View>
           </ImageBackground>
-          {/* <Text style={{ paddingLeft: scale(20), paddingBottom: verticalScale(4), color: COLOR.ORANGE, fontSize: scale(14) }}>Pesquisar</Text>
-            <Content searchBar style={Styles.searchBar}>
+          <View>
+            <Text style={{ paddingLeft: scale(20), paddingBottom: verticalScale(4), color: COLOR.ORANGE, fontSize: scale(14) }}>Pesquisar</Text>
+            <View searchBar style={Styles.searchBar}>
                 <Item>
                     <DatePicker
                         defaultDate={new Date()}
@@ -68,22 +67,23 @@ export function AlertaScreen() {
                         disabled={false}
                     />
                 </Item> 
-          </Content> */}
-          {alertas && alertas.map(alerta => {   
-            return (
-              <Alertas
-                data={`${alerta.data} - ${capitalize(alerta.dia.split('-')[0])}`}
-                startDate={alerta.horaInicio}
-                endDate={alerta.horaFim}
-                motivo={alerta.motivo}
-              />
-          )})}
-          <Loader
-            open={open}
-          />
+            </View>
+          </View>
+          <View>
+            <FlatList
+              data={alertas}
+              renderItem={({ item }) => (
+                <Alertas
+                    data={`${item.data} - ${capitalize(item.dia.split('-')[0])}`}
+                    startDate={item.horaInicio}
+                    endDate={item.horaFim}
+                    motivo={item.motivo}
+                />
+              )}
+            />
+          </View>
           <ImageBackground source={require('../../assets/borda-topo.png')} style={{ width: Dimensions.get('window').width, height: 100, marginTop: -30 }}/>
           <ImageBackground source={require('../../assets/ship-opacity.png')} style={{ width: Dimensions.get('window').width, height: 100, marginTop: -30 }}/>
-          </Content>
       </Container>
   )
 }
